a69e89e36233bdf8527bf76217e085bc81c4574e
Barry Shovlin <bshovlin32@gmail.com>
2021-05-05 11:07:45 -0500

    all userProfiles are showing in list, working on details to show when clicked

--- Here To Help/client/src/Header.js
+++ Here To Help/client/src/Header.js
@@ -19,7 +19,7 @@ export default function Header() {
     return (
         <div>
             <Navbar className="nav-bar-link-container" color="light" light expand="md">
-                <NavbarBrand tag={RRNavLink} to="/">Tabloid</NavbarBrand>
+                <NavbarBrand tag={RRNavLink} to="/">Here To Help</NavbarBrand>
                 <NavbarToggler onClick={toggle} />
                 <Collapse isOpen={isOpen} navbar>
                     <Nav className="mr-auto" navbar>
