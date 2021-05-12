b97d07be63ba3a6b2f5d2cd2d5b0750dbf5c8920
Barry Shovlin <bshovlin32@gmail.com>
2021-05-11 22:09:02 -0500

    added all server side for skillTags

---
+++ Here To Help/Repositories/ISkillTagRepository.cs
@@ -0,0 +1,13 @@
+﻿using Here_To_Help.Models;
+using System.Collections.Generic;
+
+namespace Here_To_Help.Repositories
+{
+    public interface ISkillTagRepository
+    {
+        void Add(SkillTag skillTag);
+        void Delete(int SkillTagId);
+        List<SkillTag> GetAllSkillTags();
+        SkillTag GetSkillTagById(int id);
+    }
+}
